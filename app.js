57e7deb82632ea3953022869fd2c210c73565403
sawfiz <dashu.up@gmail.com>
2023-09-05 20:41:12 -0400

    Pass title into index.ejs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {title: 'Mini Message Board'})
 })
 
 app.listen(3000)
