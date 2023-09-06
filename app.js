642836539f18ee499e0a6e4553631b91ab8f9fa8
sawfiz <dashu.up@gmail.com>
2023-09-05 20:36:07 -0400

    Use ejs as view engine to render index.ejs

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Mini Message Board')
+  res.render('index')
 })
 
 app.listen(3000)
