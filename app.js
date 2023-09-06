a7397457700a53442f328bb8be06a3aa68f710de
sawfiz <dashu.up@gmail.com>
2023-09-06 14:18:16 -0400

    Add indexRouter

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('index', {title: 'Mini Message Board'})
-})
+// app.get('/', (req, res) => {
+//   res.render('index', {title: 'Mini Message Board'})
+// })
+
+const indexRouter = require('./routes/index')
+
+app.use('/', indexRouter)
 
 app.listen(3000)
